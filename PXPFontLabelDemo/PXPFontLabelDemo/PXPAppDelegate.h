5b49416316ef4c18f708ea2f925a1340f33ccada
Paris Xavier Pinkney <paris@pxpgraphics.com>
2014-07-21 12:45:43 -0700

    Initial commit.

---
+++ PXPFontLabelDemo/PXPFontLabelDemo/PXPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PXPAppDelegate.h
+//  PXPFontLabelDemo
+//
+//  Created by Paris Pinkney on 7/21/14.
+//  Copyright (c) 2014 PXPGraphics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PXPFontLabelDemo/PXPFontLabelDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PXPFontLabelDemo
+//
+//  Created by Paris Pinkney on 7/21/14.
+//  Copyright (c) 2014 PXPGraphics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PXPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXPAppDelegate class]));
+	}
+}
